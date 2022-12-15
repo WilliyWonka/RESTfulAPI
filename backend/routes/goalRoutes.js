f4493de66e02227c1aa5c6cef347b0a75d7001da
WilliyWonka <numberhwg@gmail.com>
2022-12-15 15:23:17 +0900

    Added JWT and Frontend REDUX

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,15 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
+const {getGoals,
+     setGoal,
+     updateGoal,
+     deleteGoal,
+    } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'Get goals'})
-})
+const {protect} = require('../middleware/authMiddleware')
 
-module.exports = router
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+
+
+module.exports = router 
